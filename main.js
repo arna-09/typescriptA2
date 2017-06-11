0479e63f47d68a97b20c78cc9800a798fb2c1531
arna-09 <arna.irks@gmail.com>
2017-06-11 23:29:25 +0530

    typescript fat arrow

---
+++ main.js
@@ -0,0 +1,2 @@
+var addition = function (x, y) { console.log("String value is : " + x + " and number is : " + y); };
+addition('abcd', 4);
---
+++ main.ts
@@ -0,0 +1,5 @@
+
+
+var addition : string = (x:string,y:number) => {console.log(`String value is : ${x} and number is : ${y}`);};
+addition('abcd',4);
+
